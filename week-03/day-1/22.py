01a9f0db3c9ef2942b7967701a1cd79ae1ae5412
Máté Szurovecz <szuri92@gmail.com>
2016-11-02 17:11:11 +0100

    add easier solution for 22.py

--- week-03/day-1/22.py
+++ week-03/day-1/22.py
@@ -1,7 +1,6 @@
 v = 426
 # print 'Yeah!' if dividable by 4 but print 'End of program' after regardless
-if v % 3 == 0:
+if v % 4 == 0:
     print("Yeah")
-    print("End of program")
-else:
-    print("End of Program")
+
+print("End of Program")
