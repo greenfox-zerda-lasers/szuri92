47c81f48870a56e9182d2fb4ab69eb4149bc0e6b
Máté Szurovecz <szuri92@gmail.com>
2016-11-14 14:01:00 +0100

    add files for day-1 exercise 1

---
+++ week-05/day-1/szurovecz_mate_test.py
@@ -0,0 +1,34 @@
+from szurovecz_mate_work import anagramm, count_letters
+import unittest
+#from collections import Counter
+
+class TestMyFunctions(unittest.TestCase):
+    def test_if_simple_strings_true(self):
+        self.assertTrue(anagramm('lajos', 'solja'), True)
+    def test_if_lower_and_upper_equal(self):
+        self.assertTrue(anagramm('lajos', 'SoJla'))
+    def test_if_normal_and_spaced_is_true(self):
+        self.assertFalse(anagramm('lajos', 'Llojas'))
+    def test_if_int_false(self):
+        self.assertRaises(AttributeError, anagramm, 'lajos', 15)
+
+
+    def test_if_count_upper_letter(self):
+        self.assertEqual(count_letters('BElLLLaaA'), ({'b' : 1, 'e': 1, 'l' : 4, 'a': 3}))
+    def test_count_letter_easy_string(self):
+        self.assertEqual(count_letters('Jozsi'), {'j' : 1, 'o': 1, 'z' : 1, 's': 1, 'i' : 1})
+    def test_count_letter_empty_string(self):
+        self.assertEqual(count_letters(''), {})
+    def test_count_for_non_letter(self):
+        self.assertEqual(count_letters('be12@la!'), {'b': 1, 'a': 1, 'e' : 1, 'l': 1})
+    def test_count_for_int(self):
+        self.assertRaises(AttributeError, count_letters, 12)
+    def test_count_for_list(self):
+        self.assertRaises(AttributeError, count_letters, [1, 1, 2, 3, 5, 7, 'a', 'v'])
+    def test_count_for_list(self):
+        self.assertRaises(AttributeError, count_letters, ['a', 'v', 'c'])      
+
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
---
+++ week-05/day-1/szurovecz_mate_work.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+# return True if string1 and string2 are anagramms
+def anagramm(string1, string2):
+    new_sorted1 = ''
+    new_sorted2= ''
+    string1 = list(string1.lower())
+    string2 = list(string2.lower())
+    string1.sort()
+    string2.sort()
+    for i in range(len(string1)):
+        if string1[i] != ' ':
+            new_sorted1 += string1[i]
+    for i in range(len(string2)):
+        if string2[i] != ' ':
+            new_sorted2 += string2[i]
+    return new_sorted1 == new_sorted2
+
+#return a dictionary with letters in string, and numbers as occurences
+def count_letters(string):
+    sorted_list= ''
+    string1 = string.lower()
+    for i in range(len(string1)):
+        if ord(string1[i]) in range (97, 123):
+            sorted_list += string1[i]
+    sorted_list = Counter(sorted_list)
+    if sorted_list == '':
+        return {}
+    else:
+        return sorted_list
