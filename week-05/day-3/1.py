1ff4479ff17e13cd64b18074528a8021dc545ac8
Máté Szurovecz <szuri92@gmail.com>
2016-11-16 10:50:58 +0100

    add files for day-3

---
+++ week-05/day-3/1.py
@@ -0,0 +1,13 @@
+# create a function that takes a number and divides ten with it and prints the result
+# it should print "fail" if it is divided by 0
+
+
+def divide_ten(divider):
+    try:
+        return 10/divider
+    except ZeroDivisionError:
+        print('fail')
+
+
+
+print(divide_ten(21))
