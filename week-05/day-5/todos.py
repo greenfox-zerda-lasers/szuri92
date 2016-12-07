3b443bf607afedbdd08743adccc0dfb42daf5c42
Máté Szurovecz <szuri92@gmail.com>
2016-12-07 17:07:51 +0100

    remove 'filefunctions comment

--- week-05/day-5/todos.py
+++ week-05/day-5/todos.py
@@ -1,12 +1,13 @@
 import csv
 import sys
 import os
-class ToDoApp:
+
+class ToDoApp():
 
     def __init__(self):
-            self.file_name = 'C:/Users/Gáspár/Desktop/Greenfox/szuri92/week-05/day-5/tasks.csv'
-            self.mode = sys.argv
-            self.loop()
+        self.file_name = 'C:/Users/Gáspár/Desktop/Greenfox/szuri92/week-05/day-5/tasks.csv'
+        self.mode = sys.argv
+        self.loop()
 
     def file_opener(self, permission):
         try:
@@ -18,15 +19,15 @@ class ToDoApp:
         return f
 
     def csv_to_list(self):
-            f = self.file_opener('r')
-            csv_f = csv.reader(f, delimiter = ';')
-            lista = list(csv_f)
-            f.close
-            return lista
+        f = self.file_opener('r')
+        csv_f = csv.reader(f, delimiter = ';')
+        lista = list(csv_f)
+        f.close()
+        return lista
 
     def csv_delete(self):
         f = self.file_opener('w')
-        csv_w = csv.writer(f)
+        csv.writer(f)
         f.close()
 
     def write_task(self, a_task):
@@ -51,7 +52,6 @@ class ToDoApp:
                 #print( str( i + 1 ) + ' - [ ] ' + lista[i][1])
                 print('{} - [ ] {}'.format(i+1, lista[i][1]))
 
-
     def delete(self, number):
         lista = self.csv_to_list()
         if number in range(1, len(lista)+1):
@@ -73,17 +73,16 @@ class ToDoApp:
                 if i == number-1:
                     lista[i][0] = 'done'
             c = self.file_opener('a')
-            csv_a = csv.writer(c, delimiter =';', lineterminator = '\n')
+            csv_a = csv.writer(c, delimiter = ';', lineterminator = '\n')
             for i in range(len(lista)):
                 csv_a.writerow(lista[i])
             c.close()
         else:
             print('Unable to check: Index is out of bound')
-##### FILEFUNCTIONS ABOVE
 
     def loop(self):
         if len(self.mode) <= 1:
-            os.system('cls' if os.name=='nt' else 'clear')
+            os.system('cls' if os.name == 'nt' else 'clear')
             print('Python Todo application')
             print('=======================\n')
             print('Command line arguments:')
@@ -93,7 +92,7 @@ class ToDoApp:
             print(' -c   Completes an task')
         elif len(self.mode) == 2:
             if self.mode[1] == '-l':
-                os.system('cls' if os.name=='nt' else 'clear')
+                os.system('cls' if os.name == 'nt' else 'clear')
                 self.display_list()
             elif self.mode[1] == '-a':
                 print('Unable to add: No task is provided')
@@ -104,21 +103,19 @@ class ToDoApp:
             else:
                 print('Unsupported argument')
         elif len(self.mode) > 2:
-            if self.mode[1] == '-a' and self.mode[2] !='' and len(self.mode) > 2:
+            if self.mode[1] == '-a' and self.mode[2] != '' and len(self.mode) > 2:
                 for i in range(3, len(self.mode)):
                     self.mode[2] += ' ' + self.mode[i]
                 self.write_task(self.mode[2])
-            elif self.mode[1] == '-r' and self.mode[2] !='':
+            elif self.mode[1] == '-r' and self.mode[2] != '':
                 try:
                     self.delete(int(self.mode[2]))
                 except ValueError:
                     print('Unable to remove: Index is not a number')
-            elif self.mode[1] == '-c' and self.mode[2] !='':
+            elif self.mode[1] == '-c' and self.mode[2] != '':
                 try:
                     self.check_task(int(self.mode[2]))
                 except ValueError:
                     print('Unable to check: Index is not a number')
 
-
-
-valami = ToDoApp()
+application1 = ToDoApp()
