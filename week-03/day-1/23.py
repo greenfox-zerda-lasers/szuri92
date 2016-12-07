6f45a9ebf2cf1ef7256ca749a99d43f868b7b5f8
Máté Szurovecz <szuri92@gmail.com>
2016-12-07 16:24:37 +0100

    indentation

--- week-03/day-1/23.py
+++ week-03/day-1/23.py
@@ -5,3 +5,4 @@ out = 0
 if w % 2 == 0:
     out+=1
     print(out)
+    
--- week-03/day-1/38.py
+++ week-03/day-1/38.py
@@ -2,15 +2,15 @@
 # But for multiples of three print "Fizz" instead of the number
 # and for the multiples of five print "Buzz".
 # For numbers which are multiples of both three and five print "FizzBuzz".
-i=1
 
-while i < 100:
-    if i % 5 == 0 and i % 3 !=0:
-        print("Buzz")
-    elif i % 3 == 0 and i % 5 != 0:
-        print("Fizz")
-    elif i % 3 == 0 and i % 5 == 0:
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Buzz")
+    elif i % 5 == 0:
+        print("Fizz")
     else:
         print(i)
     i+=1
