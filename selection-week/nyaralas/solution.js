2731c3f6d961b67469fc55aa2844a0369ba05363
Máté Szurovecz <szuri92@gmail.com>
2017-02-27 14:47:27 +0100

    add insertItem function

--- selection-week/nyaralas/solution.js
+++ selection-week/nyaralas/solution.js
@@ -8,8 +8,24 @@ var createArray = function (obj) {
    return result
 }
 
+var insertItem = function (array, item, index) {
+  var result = [];
+  result = array.splice(index, 0, item);
+  return array;
+}
+
+var createDestination = function(listOfObj) {
+  var destination = [];
+  for (var i = 0; i < listOfObj.length; i++) {
+    if (destination.indexOf(listOfObj[i].name) === -1 && destination.indexOf[i].depende)
+    var depArray = createArray(listOfObj[i]);
+    destination.push(depArray[1], depArray[0]);
+  }
+}
+
 return {
-  createArray: createArray
+  createArray: createArray,
+  insertItem: insertItem
 };
 
 })();
--- selection-week/nyaralas/test.js
+++ selection-week/nyaralas/test.js
@@ -7,3 +7,17 @@ test('createArray', function (t) {
     t.deepEqual(actual, expected);
     t.end();
 });
+
+test('insertToArray', function (t) {
+    var actual = solution.insertItem(['a', 'b', 'c'], 'x', 0);
+    var expected = ['x','a', 'b', 'c'];
+    t.deepEqual(actual, expected);
+    t.end();
+});
+
+test('insertToArray', function (t) {
+    var actual = solution.insertItem(['a', 'b', 'c'], 'x', 1);
+    var expected = ['a', 'x', 'b', 'c'];
+    t.deepEqual(actual, expected);
+    t.end();
+});
